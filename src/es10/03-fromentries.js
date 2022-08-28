f581b8b2b59726f23ec27c6c51e11634ba515650
Juan Villa <ing.villa.jimenez@gmail.com>
2022-08-28 18:02:57 -0500

    curso terminado

---
+++ src/es10/03-fromentries.js
@@ -0,0 +1,7 @@
+const entries = new Map([
+  ["name", "oscar"],
+  ["age", 34],
+]);
+
+console.log(entries);
+console.log(Object.fromEntries(entries));
---
+++ src/es6/02-strings.js
@@ -0,0 +1,18 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world + "!";
+console.log(epicPhrase);
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = "es es un string \n " + "esto es otra linea";
+
+let lorem2 = `Esta es una frase epica
+ la continuacion de esa frase epica.
+`;
+
+console.log(lorem);
+console.log(lorem2);
